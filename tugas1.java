b503d9ba81de38f860f817cd2b21e22c15c7a628
FikriALFayyedh <alfayyedhfikri@gmail.com>
2022-09-07 21:31:42 +0800

    1

---
+++ tugas1.java
@@ -0,0 +1,12 @@
+public class tugas1 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("NIM        : 2110131210014");
+        System.out.println("Nama       : FIKRI AL FAYYEDH");
+        System.out.println("Alamat     : Komp.Sejahtera Mandiri Asri,KM 14.400, Gambut");
+        System.out.println("Hobi       : game ");
+        System.out.println("MK dikuasai: pemograman web 1");
+        System.out.println("");
+    }
+}
