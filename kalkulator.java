9be01a4ef6c88deaef6bcb7bde3fa19033b80ee2
FikriALFayyedh <alfayyedhfikri@gmail.com>
2022-09-25 23:13:56 +0800

    1

--- kalkulator.java
+++ kalkulator.java
@@ -3,14 +3,16 @@ import java.util.Scanner;
 public class kalkulator {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int numbers1, numbers2, choice, result= 0;
+        int numbers1, numbers2, result= 0;
+        String choice;
+
         System.out.println("-----------------------------");
         System.out.println("Kalkulator Sederhana");
         System.out.println("-----------------------------");
-        System.out.println("1. Penjumlahan");
-        System.out.println("2. Pengurangan");
-        System.out.println("3. Pembagian");
-        System.out.println("4. Perkalian");
+        System.out.println("1. Penjumlahan + ");
+        System.out.println("2. Pengurangan - ");
+        System.out.println("3. Pembagian   / ");
+        System.out.println("4. Perkalian   x");
         System.out.println("-----------------------------");
  
         // input data
@@ -21,15 +23,15 @@ public class kalkulator {
  
         // Pilihan Operasi
         System.out.print("Masukan Operasi: ");
-        choice = input.nextInt();
+        choice = input.next();
  
         switch (choice)
         {
-            case 1 : result = numbers1 + numbers2; break;
-            case 2 : result = numbers1 - numbers2; break;
-            case 3 : result = numbers1 / numbers2; break;
-            case 4 : result = numbers1 * numbers2; break;
-            default : System.out.println("Salah Memasukan Pilihan Harap Baca Pilihan di Menu");
+            case "+" : result = numbers1 + numbers2; break;
+            case "-" : result = numbers1 - numbers2; break;
+            case "/" : result = numbers1 / numbers2; break;
+            case "x" : result = numbers1 * numbers2; break;
+            default : System.out.println("Operator tidak tersedia");
         }
         System.out.println("Hasil: " + result);
         System.out.println("-----------------------------");
